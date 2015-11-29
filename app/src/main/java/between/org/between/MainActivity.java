1f38bdb1a4532230f18c383ed9c2b42a466e9399
Nivetha <nivetha.sathyarajan@gmail.com>
2015-11-29 06:30:56 +0530

    updated code to get user id info
    Now the app is able to get more details about the logged in user like
    name, email id, etc.

--- app/src/main/java/between/org/between/MainActivity.java
+++ app/src/main/java/between/org/between/MainActivity.java
@@ -1,18 +1,99 @@
 package between.org.between;
 
 import android.support.v7.app.ActionBarActivity;
+import java.lang.Object;
+import org.json.JSONObject;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Arrays;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.LoginResult;
+import com.facebook.FacebookCallback;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.FacebookException;
+import android.content.Intent;
+import org.json.JSONException;
 
 public class MainActivity extends ActionBarActivity {
+
+    private LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions("user_friends");
+
+        callbackManager = CallbackManager.Factory.create();
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                Log.e("LoginActivity", response.toString() + "    " + object.toString());
+
+                                try {
+                                    String name=object.getString("name");
+                                    String email=object.getString("email");
+                                    String id=object.getString("id");
+                                    String gender=object.getString("gender");
+                                    Log.e("LoginActivity", name + "    " + email + "   " + id + "   " + gender);
+
+                                    TextView t= (TextView) findViewById(R.id.textView);
+                                    t.append((CharSequence) name);
+                                }
+                                catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+
+
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+        }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.v("LoginActivity", "cancel");
+            }
+
+            @Override
+        public void onError(FacebookException exception) {
+                // App code
+                Log.v("LoginActivity", exception.getCause().toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
